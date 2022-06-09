be53ce4b9073b5455e467b3740d92b400b787bd1
Suleenpb <suleen.goh@gmail.com>
2022-06-09 12:46:27 +0800

    add

--- src/components/TwentyFourHourForecast.js
+++ src/components/TwentyFourHourForecast.js
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from "react";
 import dateFormat from "dateformat";
 import API from "../API";
+import '../styles/TwentyFourHourWeather.css';
 
-const now = new Date();
+//const now = new Date();
 
 const TwentyFourHourForecast = (props) => {
 
     const result = props.region;
     const [forecast, setForecast] = useState('');
+    const [inputForecast, setInputForecast] = useState([]);
     const [time, setTime] = useState([]);
     
     const currentWeather = async () => {
         const { status, data } = await API.get('/environment/24-hour-weather-forecast');
-        const apiForecast = data.items[0].periods[0].regions;
+        const apiForecast = data.items[0].periods[2].regions;
+        const inputForecast = data.items[0].periods[2]
         const apiTimestamp = data.items[0].timestamp;
+
+
 
         if (status === 200) {
             switch(result){
@@ -31,6 +36,7 @@ const TwentyFourHourForecast = (props) => {
 
                 case "west":
                 setForecast(apiForecast.west);
+                break;
 
                 case "central":
                 setForecast(apiForecast.central);
@@ -39,63 +45,41 @@ const TwentyFourHourForecast = (props) => {
             setTime(apiTimestamp);
             return status;
         }
-        
-        
+        setInputForecast(inputForecast);
     }
-
-
+    
+    var weatherIcon; 
+    switch(true) {
+        case (inputForecast.includes("thundery showers")):
+            weatherIcon = "thunderstorm-24hr";
+            break;
+        case (inputForecast.includes("fair")):
+            weatherIcon = "sunny-24h";
+            break;
+        case (inputForecast.includes("cloudy")):
+            weatherIcon = "cloudy-24h";
+            break;
+        default:
+            weatherIcon = '';
+            break;
+    }
 
     useEffect(() => {
         currentWeather();
-        console.log("Region and Forecast:", result, forecast);
     }, [result]);
 
 
+
         return (
-            <>
+            <div>
             <h3>24-Hour Weather Forecast</h3>
+            <div className='weather-icon-24hr' id={weatherIcon}></div>
             <h3>Morning</h3>
             {result}
             <br></br>
             {forecast}
-
-            {/* <div>{dateFormat(now, "dddd, mmmm dS yyyy, h:MM:ss TT")}</div>
-            <ul>
-            <h4>North Region</h4>
-                {forecast.map((o) => {
-                    return <li>{dateFormat(o.time.start, "h:MM TT")} to {dateFormat(o.time.end, "h:MM TT")} {o.regions.north}
-                    </li>
-                })}
-            </ul>
-            <ul>
-            <h4>South Region</h4>
-                {forecast.map((o) => {
-                    return <li>{dateFormat(o.time.start, "h:MM TT")} to {dateFormat(o.time.end, "h:MM TT")} {o.regions.south}
-                    </li>
-                })}
-            </ul>
-            <ul>
-            <h4>Central Region</h4>
-                {forecast.map((o) => {
-                    return <li>{dateFormat(o.time.start, "h:MM TT")} to {dateFormat(o.time.end, "h:MM TT")} {o.regions.central}
-                    </li>
-                })}
-            </ul>
-            <ul>
-            <h4>East Region</h4>
-                {forecast.map((o) => {
-                    return <li>{dateFormat(o.time.start, "h:MM TT")} to {dateFormat(o.time.end, "h:MM TT")} {o.regions.east}
-                    </li>
-                })}
-            </ul>
-            <ul>
-            <h4>West Region</h4>
-                {forecast.map((o) => {
-                    return <li>{dateFormat(o.time.start, "h:MM TT")} to {dateFormat(o.time.end, "h:MM TT")} {o.regions.west}
-                    </li>
-                })}
-            </ul> */}
-            </>
+            <br></br>
+            </div>
         );
     
 
